2b419fe0c44028c6403dc11fac433a6c8055d31e
Leo Jiang <leojiang99999@gmail.com>
2025-10-19 18:15:47 -0400

    navbar logged-in view

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,24 @@ import { Button } from "@/components/ui/button";
 import { Home } from "lucide-react";
 import { Link } from "react-router-dom";
 
-export default function NavbarButtons() {
+import { useState } from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className="bg-zinc-900 w-full flex items-center p-2 justify-between">
       {/* Home button */}
@@ -12,15 +29,45 @@ export default function NavbarButtons() {
         </Link>
       </Button>
 
-      {/* Login & Register button*/}
-      <div className="flex gap-1">
-        <Button variant="outline" asChild>
-          <Link to="/login">Login</Link>
-        </Button>
-        <Button asChild>
-          <Link to="/register">Register</Link>
-        </Button>
-      </div>
+      {/* Conditionally render UI based on login state */}
+      {isLoggedIn ? (
+        // LOGGED-IN
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="relative h-9 w-9 rounded-full cursor-pointer">
+              <Avatar className="h-9 w-9">
+                <AvatarImage />
+                <AvatarFallback>?</AvatarFallback>
+              </Avatar>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="min-w-40" align="end">
+            <DropdownMenuLabel>
+              <div className="flex flex-col gap-1">
+                <p className="text-sm">Username</p>
+                <p className="text-xs text-muted-foreground">user@example.com</p>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link to="/edit">Edit Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={handleLogout} className="text-red-400 focus:text-red-400">
+              Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        // LOGGED OUT
+        <div className="flex gap-1">
+          <Button variant="outline">
+            <Link to="/login">Login</Link>
+          </Button>
+          <Button>
+            <Link to="/register">Register</Link>
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
