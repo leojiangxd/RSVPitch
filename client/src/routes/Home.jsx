0eb97301dd95d37f32f1c915d1c53faf6b85c861
Leo Jiang <leojiang99999@gmail.com>
2025-10-19 21:05:38 -0400

    home background

--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Searchbar from "@/components/Searchbar";
+import homeBg from "../assets/home.jpg";
 
 const Home = () => {
   return (
-    <div className="w-[100wh] h-[100vh] flex flex-col">
+    <div
+      className="relative w-[100vw] h-[100vh] bg-zinc-800 flex flex-col bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${homeBg})`,
+      }}
+    >
       <Navbar />
-      <div className="bg-zinc-800 flex-1 flex flex-col justify-center gap-1">
+      <div className="bg-black/60 flex-1 flex flex-col justify-center gap-1">
         <div className="text-6xl font-mono font-bold flex justify-center">RSVPitch</div>
         <Searchbar widthStyle="max-w-[50%]" />
       </div>
